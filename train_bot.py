a376f6287d6476966f16ed2b2379f4d5f995bab1
profAndreaFranca <dejandreafranca@gmail.com>
2024-05-07 21:22:58 -0300

    Co-authored-by: PedroGsla <PedroGsla@users.noreply.github.com> Co-authored-by: koinskii <koinskii@users.noreply.github.com> Co-authored-by: Brunaaa10 <Brunaaa10@users.noreply.github.com>

--- train_bot.py
+++ train_bot.py
@@ -17,8 +17,17 @@ ignore_words = ['?','!',',','.',"'s","'m"]
 train_data_file = open("intents.json").read()
 intents = json.loads(train_data_file)
 
+#Aula 137 e 138
+
 #função para anexar palavras-tronco🗿🍷
+
+    
+        
+       
+        
+
 def getStemWord(words,ignore_words) :
+    
     stemWords = [] 
     for word in words :
         if word not in ignore_words :
@@ -28,10 +37,12 @@ def getStemWord(words,ignore_words) :
 
 
 for intent in intents["intents"]:
+        # Adicione todas as palavras dos padrões à lista
         for pattern in intent["patterns"]:
                 pattern_word = nltk.word_tokenize(pattern)
                 words.extend(pattern_word)
                 word_tags_list.append((pattern_word,intent["tag"]))
+        # Adicione todas as tags à lista classes
         if intent["tag"] not in classes :
                 classes.append(intent["tag"])
                 stem_words = getStemWord(words,ignore_words)   
@@ -40,7 +51,7 @@ print(word_tags_list[0])
 print(classes)
          
         
-        
+#Crie o corpus de palavras para o chatbot        
 def CreateBotCorpus(stemWords,classes) :
       stemWords = sorted(list(set(stemWords)))
       classes = sorted(list(set(classes)))
@@ -53,10 +64,60 @@ print("****************************")
 print(stem_words)
 print(classes)
 
-    
-        # Adicione todas as palavras dos padrões à lista
-       
-        # Adicione todas as tags à lista classes
+
+traning_data = []
+number_of_tags = len(classes)
+labels = [0] * number_of_tags #[0,0,0,0]🗿🍷
+
+# Para criar um saco de palavras, execute um loop
+# for em word_tags_list. Cada elemento é chamado
+# de word_tags.
+#       ● Para cada elemento, criaremos um array de
+#       zeros e números um.
+#       ● Defina um array vazio com o nome
+#       bag_of_words.
+#       ● Em seguida, defina pattern_words
+#       (palavras-padrão) como o primeiro índice de
+#       word_tags.
+
+#       ● Execute um loop for em pattern_words
+#       para encontrar suas palavras-tronco.
+
+#       ● Se essas palavras estiverem presentes nas
+#       palavras-tronco, anexaremos 1 ao
+#       bag_of_words; caso contrário, anexaremos 0.
+
+#       ● Reproduza o bag_of_words para verificar o
+#       resultado.
+for  word_tags in word_tags_list:
+        bag_of_words = []
+        pattern_words = word_tags[0]
+        for word in pattern_words: 
+              index = pattern_words.index(word)
+              word = stemmer.stem(word.lower())
+              pattern_words[index] = word
+
+        for word in stem_words: 
+                if word in pattern_words: 
+                        bag_of_words.append(1)
+                else: 
+                        bag_of_words.append(0)
+        print(bag_of_words)
+# Criando a label_encoding (codificação de etiquetas):        
+        labels_encoding = list(labels)
+        tag = word_tags[1]
+        index=classes.index(tag)
+        labels_encoding[index] = 1
+        traning_data.append([bag_of_words,labels_encoding])
 
 
-#Crie o corpus de palavras para o chatbot
+print(traning_data[0])
+
+def preprocess_traning_data(traning_data) :
+       traning_data = np.array(traning_data,dtype = object)
+       traning_X = list(traning_data[:,0])
+       traning_Y = list(traning_data[:,1])
+       print(traning_X[0],traning_Y[0])
+       return(traning_X,traning_Y)
+
+traning_X, traning_Y = preprocess_traning_data(traning_data)
